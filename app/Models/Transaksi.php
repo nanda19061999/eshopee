7a474a1a711e1c3ecc8f163f9876b8c219d01289
nanda19061999 <anandamardhatillah23@gmail.com>
2020-11-13 13:46:57 +0700

    Ananda Mardhatillah

---
+++ app/Models/Transaksi.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaksi extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transaksi';
+    public $timestamps = false;
+
+    public function transaksiDetils(){
+        return $this->hasMany('App\Models\TransaksiDetil');
+    }
+}
